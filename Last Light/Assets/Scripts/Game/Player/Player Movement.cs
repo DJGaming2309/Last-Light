33b2a237c58015cfd6afbcd46145fecf16fc0309
DJGaming2309 <deangordon56@gmail.com>
2024-05-03 10:33:36 +0100

    upload game files

---
+++ Last Light/Assets/Scripts/Game/Player/Player Movement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    private Rigidbody2D _rigidbody2;
+    private Vector2 _movementInput;
+
+    private void Awake()
+    {
+        _rigidbody2 = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        _rigidbody2.velocity = _movementInput* _speed;
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        _movementInput = inputValue.Get<Vector2>();  
+    }
+
+
+
+}
