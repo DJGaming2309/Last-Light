774216d6828c1080956d97553cefdc4beb4db0a6
DJGaming2309 <deangordon56@gmail.com>
2024-05-08 12:31:49 +0100

    Added new scripts

---
+++ Last Light/Assets/Scripts/Game/Enemy/EnemyMovement.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private float _rotationSpeed;
+
+    [SerializeField]
+    private float _screenBorder;
+
+    private Rigidbody2D _rigidbody;
+    private PlayerAwarenessController _playerAwarenessController;
+    private Vector2 _targetDirection;
+    private float _changeDirectionCooldown;
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _playerAwarenessController = GetComponent<PlayerAwarenessController>();
+        _targetDirection = transform.up;
+        _camera = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateTragetDirection();
+        RotateTowardsTarget();
+        SetVelocity();
+    }
+
+
+    private void UpdateTragetDirection()
+    {
+        HandleRandomDirectionChange();
+        HandlePlayerTrageting();
+        HandleEnemyOffScreen();
+    }
+
+    private void HandleRandomDirectionChange()
+    {
+        _changeDirectionCooldown = Time.deltaTime;
+
+        if (_changeDirectionCooldown <= 0)
+        {
+            float angleChange = Random.Range(-90f, 90f);
+            Quaternion rotation = Quaternion.AngleAxis(angleChange, transform.forward);
+            _targetDirection = rotation * _targetDirection;
+
+            _changeDirectionCooldown = Random.Range(1f, 5f);
+        }
+    }
+
+    private void HandlePlayerTrageting()
+    {
+        if (_playerAwarenessController.AwareOfPlayer)
+        {
+            _targetDirection = _playerAwarenessController.DirectionToPlayer;
+        }
+    }
+
+    private void HandleEnemyOffScreen()
+    {
+        Vector2 screenPosition = _camera.WorldToScreenPoint(transform.position);
+
+        if ((screenPosition.x < _screenBorder && _targetDirection.x < 0) ||
+             (screenPosition.x > _camera.pixelWidth - _screenBorder && _targetDirection.x > 0))
+        {
+            _targetDirection = new Vector2(-_targetDirection.x, _targetDirection.y);
+        }
+
+        if ((screenPosition.y < _screenBorder && _targetDirection.y < 0) ||
+            (screenPosition.y > _camera.pixelHeight - _screenBorder && _targetDirection.y > 0))
+        {
+            _targetDirection = new Vector2(_targetDirection.x, -_targetDirection.y);
+        }
+    }
+
+
+    private void RotateTowardsTarget()
+    {
+        if (_targetDirection == Vector2.zero)
+        {
+            return;
+        }
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _targetDirection);
+        Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+        _rigidbody.SetRotation(rotation);
+
+    }
+
+    private void SetVelocity()
+    {
+        _rigidbody.velocity = transform.up * _speed;
+    }
+
+}
